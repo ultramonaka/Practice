ff799751624227be05355dfbe7302d87a1247491
Knaka <soundjoker1988@mail.com>
2020-10-30 00:14:17 +0900

    test\

--- Practice1/Prac.py
+++ Practice1/Prac.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
+
+print("Hello World")
 
 #グラフ横軸
 x = range(1,1000)
 #Generate sin wave
-y = np.array([math.sin(math.radisans(ms)) for ms in x])
+y = np.array([math.sin(math.radians(ms)) for ms in x])
 
 
 #Graph
 plt.plot(x,y,label='sin', color='blue')
 plt.show();
+
